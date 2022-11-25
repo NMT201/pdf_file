c0a46a6f1bd0f47fe989b79cdeed7634581aee42
Minh Tri AI <trinm@unicloud.com.vn>
2022-11-25 11:07:55 +0700

    fix path shopee

--- shopee.py
+++ shopee.py
@@ -6,7 +6,7 @@ from PyPDF2 import PdfReader
 
 
 def shopee(pdf_file, df_day_du):
-    reader = PdfReader(r"pdf_file\shopee1.pdf")
+    reader = PdfReader(pdf_file.read())
     result_dict = {
         'Mã vận đơn' : [],
         'Mã đơn hàng' : [],
